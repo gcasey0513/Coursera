e727926f29cdaf0403106be338aecaf3ce6c772d
gcasey0513 <gcasey@junctionsolutions.com>
2014-10-15 10:00:31 -0600

    removed comment

--- corr.R
+++ corr.R
@@ -10,7 +10,7 @@ corr <- function(directory, threshold = 0) {
                 for(i in 1:nrow(threshold_df)) # for every file specified
                 {
                         my_data<-read.csv(files_full[threshold_df$id[i]]) #selct the file by index of ID
-                        #my_data2<-my_data[complete.cases(my_data),]
+                        
                         my_vector<-c(my_vector, as.vector(cor(my_data$sulfate, my_data$nitrate, use = "na.or.complete")))
                 }          
         }
